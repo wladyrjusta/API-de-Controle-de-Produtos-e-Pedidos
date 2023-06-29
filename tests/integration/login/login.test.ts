e3b7585bcb1a81983031965649673192e24257f4
wladyrjusta <wladyr.justa@gmail.com>
2023-06-29 17:57:11 -0300

    Testes unitários da camada controller e testes de integração post /login realizados.

--- tests/integration/login/login.test.ts
+++ tests/integration/login/login.test.ts
@@ -1,10 +1,82 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import bcrypt from 'bcryptjs';
+
+import app from '../../../src/app';
+import loginMock from '../../mocks/login.mock';
+import UserModel from '../../../src/database/models/user.model';
 
 chai.use(chaiHttp);
 
 describe('POST /login', function () { 
-  beforeEach(function () { sinon.restore(); });
-
+  beforeEach(function () {
+    sinon.restore();
+  });
+  it('ao não receber um username, retorne um erro', async function () {
+    // Arrange
+    const httpRequestBody = loginMock.noUserLoginReq;
+    // Act
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(httpRequestBody);
+    // Assert
+    expect(httpResponse.status).to.equal(400);
+    expect(httpResponse.body).to.be.deep.equal(loginMock.noUsernamePasswordError.data);
+  });
+  it('ao não receber uma senha, retorne um erro', async function () {
+    // Arrange
+    const httpRequestBody = loginMock.noPasswordLoginReq;
+    // Act
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(httpRequestBody);
+    // Assert
+    expect(httpResponse.status).to.equal(400);
+    expect(httpResponse.body).to.be.deep.equal(loginMock.noUsernamePasswordError.data);
+  });  
+  it('ao receber um username inválido/inexistente, retorne um erro', async function () {
+    // Arrange
+    const httpRequestBody = loginMock.unauthorizedUserLoginReq;
+    sinon.stub(UserModel, 'findOne').resolves(null);
+    // Act
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(httpRequestBody);
+    // Assert
+    expect(httpResponse.status).to.equal(401);
+    expect(httpResponse.body).to.be.deep.equal(loginMock.unauthorizedUserError.data);
+  });
+  it('ao receber um username válido e uma senha inválida, retorne um erro', async function () {
+    // Arrange
+    const httpRequestBody = loginMock.unauthorizedPasswordLoginReq;
+    const mockFindOneReturn = UserModel.build(loginMock.userFromDb);
+    sinon.stub(UserModel, 'findOne').resolves(mockFindOneReturn);
+    // Act
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(httpRequestBody);
+    // Assert
+    expect(httpResponse.status).to.equal(401);
+    expect(httpResponse.body).to.be.deep.equal(loginMock.unauthorizedUserError.data);
+  });
+  it('ao receber um username e uma senha válidos, retorne um token de login', async function () {
+    // Arrange
+    const httpRequestBody = loginMock.validLoginReq;
+    const mockFindOneReturn = UserModel.build(loginMock.userFromDb);
+    sinon.stub(bcrypt, 'compareSync').resolves(true);
+    sinon.stub(UserModel, 'findOne').resolves(mockFindOneReturn);
+    // Act
+    const httpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send(httpRequestBody);
+    // Assert
+    expect(httpResponse.status).to.equal(200);
+    expect(httpResponse.body).to.have.key('token');
+  });
 });
--- tests/unit/controllers/login.controller.test.ts
+++ tests/unit/controllers/login.controller.test.ts
@@ -3,6 +3,12 @@ import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
 
+import { ServiceResponse } from '../../../src/types/ServiceResponde';
+import loginController from '../../../src/controllers/login.controller';
+import loginService from '../../../src/services/login.service';
+import loginMock from '../../mocks/login.mock';
+import { Token } from '../../../src/types/Token';
+
 chai.use(sinonChai);
 
 describe('LoginController', function () {
@@ -14,5 +20,59 @@ describe('LoginController', function () {
     res.json = sinon.stub().returns(res);
     sinon.restore();
   });
-
+  it('Ao não receber um username retorna erro', async function () {
+    // Arrange
+    req.body = loginMock.noUserLoginReq;
+    const serviceResponse: ServiceResponse<Token> = { status: 'INVALID_DATA', data: loginMock.noUsernamePasswordError.data };;
+    sinon.stub(loginService, 'verifyLogin').resolves(serviceResponse);
+    // Act
+    await loginController.login(req, res);
+    // Assert
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith(loginMock.noUsernamePasswordError.data);
+  });
+ it('Ao não receber uma senha retorna erro', async function () {
+    // Arrange
+    req.body = loginMock.noPasswordLoginReq;
+    const serviceResponse: ServiceResponse<Token> = { status: 'INVALID_DATA', data: loginMock.noUsernamePasswordError.data };;
+    sinon.stub(loginService, 'verifyLogin').resolves(serviceResponse);
+    // Act
+    await loginController.login(req, res);
+    // Assert
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith(loginMock.noUsernamePasswordError.data);
+  });
+  it('Ao receber um username inexistente no banco de dados retorna erro', async function () {
+    // Arrange
+    req.body = loginMock.unauthorizedUserError;
+    const serviceResponse: ServiceResponse<Token> = { status: 'UNAUTHORIZED', data: loginMock.unauthorizedUserError.data };;
+    sinon.stub(loginService, 'verifyLogin').resolves(serviceResponse);
+    // Act
+    await loginController.login(req, res);
+    // Assert
+    expect(res.status).to.have.been.calledWith(401);
+    expect(res.json).to.have.been.calledWith(loginMock.unauthorizedUserError.data);
+  });
+  it('Ao receber um username válido e uma senha inválida retorna erro', async function () {
+    // Arrange
+    req.body = loginMock.unauthorizedPasswordLoginReq;
+    const serviceResponse: ServiceResponse<Token> = { status: 'UNAUTHORIZED', data: loginMock.unauthorizedUserError.data };
+    sinon.stub(loginService, 'verifyLogin').resolves(serviceResponse);
+    // Act
+    await loginController.login(req, res);
+    // Assert
+    expect(res.status).to.have.been.calledWith(401);
+    expect(res.json).to.have.been.calledWith(loginMock.unauthorizedUserError.data);
+  });
+  it('Ao receber um username e senha válidos, retorna um token', async function () {
+    // Arrange
+    req.body = loginMock.validLoginReq;
+    const serviceResponse: ServiceResponse<Token> = { status: 'SUCCESSFUL', data: loginMock.successLoginResponse.data };;
+    sinon.stub(loginService, 'verifyLogin').resolves(serviceResponse);
+    // Act
+    await loginController.login(req, res);
+    // Assert
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(loginMock.successLoginResponse.data);
+  });
 });
